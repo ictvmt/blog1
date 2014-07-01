c59a0e4b291e1c87f1a352e7d206fd5ae974a63f
tung <ictvmt1@gmail.com>
2014-07-01 14:56:45 +0700

    v 1.0

--- Rakefile
+++ Rakefile
@@ -3,4 +3,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-SampleBlog::Application.load_tasks
+SampleApp::Application.load_tasks
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,29 @@
-SampleBlog::Application.routes.draw do
-  resources :users
-  #get "home_page/home"
-  root  'home_page#home'
+SampleApp::Application.routes.draw do
+  
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  
+  match '/home', to:  'static_pages#home' ,via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   # The priority is based upon order of creation: first created -> highest priority.
+ 
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
